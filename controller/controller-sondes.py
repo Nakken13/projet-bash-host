d2d433f9f2de5f7573711ffdfed7d9d8ada1e0c7
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-10 19:38:50 +0100

    update clé ssh

--- controller/controller-sondes.py
+++ controller/controller-sondes.py
@@ -1,18 +1,19 @@
 #!/bin/python
 import os
+#ip = os.popen("hostname -I | awk '{print $1}'").read().strip()
 
-guests = {"nt": "azerty","user2":"azerty", "user3":"azerty", "user4":"azerty"}
+guests = {"nt": "172.20.10.2"}
 
 def recup_log(guests):
     if not os.path.exists("logs"):
         os.system("mkdir logs")
         
-    for user, passwd in guests.items():
-        print(f"[*] Connexion en cours : {user}@10.0.2.15...", flush=True)
-        ret = os.system(f"sshpass -p '{passwd}' ssh -o StrictHostKeyChecking=no -p 22 {user}@10.0.2.15 ~/projet-bash-guests/sondes/sonde.sh >> logs/log-{user}.json 2>&1")
-        if ret == 0:
+    for user, ip in guests.items():
+        print(f"[*] Connexion en cours : {user}@{ip}...", flush=True)
+        res = os.system(f"ssh {user}@{ip} ~/projet-bash-guests/sondes/sonde.sh >> logs/log-{user}.json")
+        if res == 0:
             print(f"[+] Log récupéré → logs/log-{user}.json", flush=True)
         else:
-            print(f"[-] Échec sshpass pour {user} (code: {ret})", flush=True)
+            print(f"[-] Échec sshpass pour {user} (code: {res})", flush=True)
 
 recup_log(guests)
