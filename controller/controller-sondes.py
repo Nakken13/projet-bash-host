fa32909d595f76f500048c02d256cbaf0b1d6086
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-10 12:59:59 +0100

    update verif sshpass

--- controller/controller-sondes.py
+++ controller/controller-sondes.py
@@ -8,6 +8,7 @@ def recup_log(guests):
         os.system("mkdir logs")
         
     for user, passwd in guests.items():
-        os.system(f"sshpass -p '{passwd}' ssh -p 22 {user}@10.0.2.15 ~/sondes/sonde.sh >> logs/log-{user}.json")
-        
+        os.system(f"sshpass -p '{passwd}' ssh -o StrictHostKeyChecking=no -p 22 {user}@10.0.2.15 ~/projet-bash-guests/sondes/sonde.sh >> logs/log-{user}.json 2>&1")
 recup_log(guests)
+
+
