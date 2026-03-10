360d7e6d33fb08c425d0882e1e457e6176071a39
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-11 00:18:44 +0100

    minor fix

--- controller/controller-sondes.py
+++ controller/controller-sondes.py
@@ -1,4 +1,5 @@
-#!/bin/python
+#!/usr/bin/python3
+
 import os
 #ip = os.popen("hostname -I | awk '{print $1}'").read().strip()
 
@@ -7,13 +8,13 @@ guests = {"nt": "172.20.10.2"}
 def recup_log(guests):
     if not os.path.exists("logs"):
         os.system("mkdir logs")
-        
+
     for user, ip in guests.items():
         print(f"[*] Connexion en cours : {user}@{ip}...", flush=True)
-        res = os.system(f"ssh {user}@{ip} ~/projet-bash-guests/sondes/sonde.sh >> logs/log-{user}.json")
+        res = os.system(f"ssh {user}@{ip} '~/projet-bash-guests/sondes/sonde.sh' >> logs/log-{user}.json")
         if res == 0:
-            print(f"[+] Log récupéré → logs/log-{user}.json", flush=True)
+            print(f"[+] Log récupéré -> logs/log-{user}.json", flush=True)
         else:
-            print(f"[-] Échec sshpass pour {user} (code: {res})", flush=True)
+            print(f"[-] Échec ssh pour {user} (code: {res})", flush=True)
 
 recup_log(guests)
