299e90503208aa3bd81041ea621632e467ddc0e6
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-10 12:23:54 +0100

    update logs

--- controller/controller-sondes.py
+++ controller/controller-sondes.py
@@ -1,10 +1,13 @@
+#!/bin/python
 import os
 
-guests = {"nt": "azerty","user2":"azerty", "user3":"azerty", "user4":"azerty", "user5":"azerty"}
+guests = {"nt": "azerty","user2":"azerty", "user3":"azerty", "user4":"azerty"}
 
 def recup_log(guests):
+    if not os.path.exists("logs"):
+        os.system("mkdir logs")
+        
     for user, passwd in guests.items():
-        data = os.system(f"sshpass -p '{passwd}' ssh -p 22 {user}@10.0.2.15 ~/sondes/sonde.sh")
-        os.system(f"cd logs && {data}>>log-{user}.json && cd ..")
-
+        os.system(f"sshpass -p '{passwd}' ssh -p 22 {user}@10.0.2.15 ~/sondes/sonde.sh >> logs/log-{user}.json")
+        
 recup_log(guests)
